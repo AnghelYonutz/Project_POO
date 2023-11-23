93cceeafec436eacbb95a827e1722f0a5c183f44
Ionut <ionut.anghel.2008@hotmail.com>
2023-11-23 20:09:12 +0200

    Snapshot 1

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+class Animal:
+    def __init__(self, animal, rasa, culoare, marime, domesticat):
+        self.animal = animal
+        self.rasa = rasa
+        self.culoare = culoare
+        self.marime = marime
+        self.domesticat = domesticat
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __str__(self):
+        return "Aceasta este un/o {self.animal} de rasa {self.rasa}. Are culoare {self.culoare} si e de marime {self.marime}".format(self=self)
+
+    def __eq__(self, other):
+        if type(other) == Animal:
+            return self.domesticat == other.domesticat
+        else:
+            return False
+
+a = Animal("pisica", "persana", "alba", "mica", True)
+b = Animal("caine", "beagle", "negru", "medie", True)
+c = Animal("vulpe", "fennec", "gri", "medie", False)
+
+print(a)
+print(b)
+print(c)
+
+print(a == b)
+print(b == c)
+print(c == a)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
