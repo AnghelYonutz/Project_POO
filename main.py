b8d86ed3c8a4e2e7f2080e9bf280afa4308512cf
Ionut <ionut.anghel.2008@hotmail.com>
2023-11-23 20:31:30 +0200

    Rewrite

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 class Animal:
-    def __init__(self, animal, rasa, culoare, marime, domesticat):
+    def __init__(self, animal, domeniu, regnu, branch, clasa, familia):
         self.animal = animal
-        self.rasa = rasa
-        self.culoare = culoare
-        self.marime = marime
-        self.domesticat = domesticat
+        self.domeniu = domeniu
+        self.regnu = regnu
+        self.branch = branch
+        self.clasa = clasa
+        self.familia = familia
 
     def __str__(self):
-        return "Aceasta este un/o {self.animal} de rasa {self.rasa}. Are culoare {self.culoare} si e de marime {self.marime}".format(self=self)
+        return "{self.animal} este un organism din domeniul {self.domeniu}, regnul {self.regnu}, incregatura {self.branch}, clasa {self.clasa}, din familia {self.familia}.".format(self=self)
 
     def __eq__(self, other):
         if type(other) == Animal:
-            return self.domesticat == other.domesticat
+            return self.familia == other.familia
         else:
             return False
 
-a = Animal("pisica", "persana", "alba", "mica", True)
-b = Animal("caine", "beagle", "negru", "medie", True)
-c = Animal("vulpe", "fennec", "gri", "medie", False)
+a = Animal("Vulpe", "eucariota", "animalia", "chordata", "mammalia", "canidae")
+b = Animal("Ursul brun", "eucarioata", "animalia", "chordata", "mammalia", "ursidae")
+c = Animal("Lupul", "eucariota", "animalia", "chordata", "mammalia", "canidae")
+d = Animal("Cerb", "eucariota", "animalia", "chordata", "mammalia", "cervidae")
 
 print(a)
 print(b)
 print(c)
+print(d)
 
 print(a == b)
 print(b == c)
 print(c == a)
+print(d == a)
+print(d == b)
+print(d == c)
+
+
+
 
 
 
